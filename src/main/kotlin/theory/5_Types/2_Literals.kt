e959bbdb838497cc6a160be74d9a7a6a1d45407a
Nik Ze <zebolovnik@yandex.ru>
2022-12-12 08:39:03 +1000

    Add 5_Types

---
+++ src/main/kotlin/theory/5_Types/2_Literals.kt
@@ -0,0 +1,20 @@
+package theory.`5_Types`
+
+
+/*
+
+literal - специальным символы-пометки для компилятора
+
+
+ */
+
+var unsignedInt1 = 10u // только целые числа
+
+var defaultInt = 16 // Присвоен Int по-умолчанию
+var forcedLong = 16L // L явно указывает на тип Long (без него был бы присвоен Int)
+
+var defaultDouble = 16.343 // Присвоен Double по-умолчанию
+var forcedFloat = 16.343f // f явно указывает на тип Float (без него был бы присвоен Double)
+
+
+
